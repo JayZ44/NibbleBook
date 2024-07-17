66ac48843d928944db84f9d35e109a0b7f2b8e94
JayZ44 <jazonyounge@pursuit.org>
2024-07-17 15:36:04 -0400

    added controllers folder to repo this time. my bad again lolol

---
+++ controllers/snacksController.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const snacks = express.Router();
+
+const {
+  getAllSnacks,
+  getOneSnack,
+  createSnack,
+  deleteSnack,
+  updateSnack,
+} = require("../queries/snacks");
+const { checkName, checkArtist } = require("../validations/checkSnacks");
+
+//INDEX
+snacks.get("/", async (req, res) => {
+  const allSnacks = await getAllSnacks();
+  res.status(200).json(allSnacks);
+});
+//SHOW
+snacks.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const snack = await getOneSnack(id);
+  if (snack.id) {
+    res.status(200).json(snack);
+  } else {
+    res.status(404).json({ error: "snack not found :(." });
+  }
+});
+//CREATE
+snacks.post("/new", checkName, checkArtist, async (req, res) => {
+  const newSnack = await createSnack(req.body);
+  res.json(newSnack);
+});
+//DELETE
+snacks.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedSnack = await deleteSnack(id);
+  if (deletedSnack.id) {
+    res.status(200).json(deletedSnack);
+  } else {
+    res.status(404).json({ error: "snack not found :(." });
+  }
+});
+//UPDATE
+snacks.put("/:id", checkName, checkArtist, async (req, res) => {
+  const { id } = req.params;
+  const updatedSnack = await updateSnack(id, req.body);
+  if (updatedSnack.id) {
+    res.status(200).json(updatedSnack);
+  } else {
+    res.status(404).json({ error: "snack not found :(." });
+  }
+});
+
+module.exports = snacks;
