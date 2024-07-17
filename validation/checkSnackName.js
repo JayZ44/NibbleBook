c55356283835bb4e95befbc001ba78e48b1130b5
Erimed33 <erikamedina@pursuit.org>
2024-07-17 15:43:43 -0400

    validations-check-snack-name

---
+++ validation/checkSnackName.js
@@ -0,0 +1,11 @@
+const checkSnackName = (req, res, next) => {
+    // check the body to see if it has name
+    if (req.body.name) {
+        return next()
+    } else {
+        res.status(404).json({ error: 'Name not found.'})
+    }
+}
+
+
+module.exports = { checkSnackName }
