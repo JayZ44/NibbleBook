f6177e7557545d07c4c5c4ca15ea836b1943b417
Erimed33 <erikamedina@pursuit.org>
2024-07-17 15:28:01 -0400

    seed.sql-complete

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,5 @@
 \c snacks_dev;
 
-INSERT INTO snacks (name, origin, description, rating, is_vegetarian, discovered_date, comments) VALUES ("Century egg", "Hunan, China" ),
-("Fried spiders", "Cambodia" ),
-("Cazu marzu", "Sardinia")
+INSERT INTO snacks (name, origin, description, discovered_date) VALUES ('Century egg', 'Hunan, China', 'Century eggs (Chinese: 皮蛋; pinyin: pídàn; Jyutping: pei4 daan2), also known as alkalized or preserved egg, are a Chinese egg-based culinary dish made by preserving duck, chicken, or quail eggs in a mixture of clay, ash, salt, quicklime, and rice hulls for several weeks to several months, depending on the processing method.', 1700 ),
+('Fried spiders', 'Cambodia', 'Fried spiders, or fried tarantulas, are a regional snack in Cambodia, especially in the market town of Skuon. The spiders are often bred in holes in the ground or foraged for in nearby forests, then fried in oil and sometimes rolled in sugar or garlic. Some say the spiders taste bland and like a cross between chicken and cod.', 1900 ),
+('Cazu marzu', 'Sardinia', 'Casu martzu, sometimes spelled casu marzu, and also called casu modde, casu cundídu and casu fràzigu in Sardinian, is a traditional Sardinian sheep milk cheese that contains live insect larvae.', 1909)
