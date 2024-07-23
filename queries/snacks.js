4f22f5d1542e56f0824f9e5617ac26c6749035b2
Erimed33 <erikamedina@pursuit.org>
2024-07-23 07:14:28 -0400

    added comments field to the PUT and UPDATE operations in crud

--- queries/snacks.js
+++ queries/snacks.js
@@ -21,7 +21,7 @@ const getOneSnack = async (id) => {
 const createSnack = async (snack) => {
   try {
     const newSnack = await db.one(
-      "INSERT INTO snacks (name, origin, description, rating, is_vegetarian, discovered_date) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      "INSERT INTO snacks (name, origin, description, rating, is_vegetarian, discovered_date, comments) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
       [
         snack.name,
         snack.origin,
@@ -29,7 +29,7 @@ const createSnack = async (snack) => {
         snack.rating || 0,
         snack.is_vegetarian || false,
         snack.discovered_date,
-        snack.comments
+        snack.comments,
       ]
     );
     return newSnack;
@@ -53,7 +53,7 @@ const deleteSnack = async (id) => {
 const updateSnack = async (id, snack) => {
   try {
     const updatedSnack = await db.one(
-      "UPDATE snacks SET name=$1, origin=$2, description=$3, rating=$4, is_vegetarian=$5, discovered_date=$6 WHERE id=$7 RETURNING *",
+      "UPDATE snacks SET name=$1, origin=$2, description=$3, rating=$4, is_vegetarian=$5, discovered_date=$6 comments=$7 WHERE id=$8 RETURNING *",
       [
         snack.name,
         snack.origin,
@@ -61,6 +61,7 @@ const updateSnack = async (id, snack) => {
         snack.rating,
         snack.is_vegetarian,
         snack.discovered_date,
+        snack.comments,
         id,
       ]
     );
